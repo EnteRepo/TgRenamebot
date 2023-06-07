cfb7941068d0bb14ac0ba545335ef0d58d197ade
༺✦҈͜͡➳ιgиιтє∂ мιи∂ѕ࿐༻ <91360737+OO7ROBot@users.noreply.github.com>
2023-06-07 08:26:12 +0530

    Update display_progress.py
    Default speed code

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -37,7 +37,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
     #if round(diff % 2.00) > 0.999 or current == total:
         # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
-        speed = current / diff * 10
+        speed = current / diff
        # speed = current / diff * 5 + 12
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
