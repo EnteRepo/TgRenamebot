0d1c9e3ea8b4030ef38dc247d40913c0a13766da
༺✦҈͜͡➳ιgиιтє∂ мιи∂ѕ࿐༻ <91360737+OO7ROBot@users.noreply.github.com>
2023-06-18 11:27:36 +0530

    Update admin.py
    Def readable time

--- plugins/admin.py
+++ plugins/admin.py
@@ -55,7 +55,23 @@ class Database:
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
     
-    
+def readable_time(seconds: int) -> str:
+    result = ''
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result    
 
 StartTime  = time.time()
 
